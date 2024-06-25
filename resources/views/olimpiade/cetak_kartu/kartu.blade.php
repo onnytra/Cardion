f1813753298380edfb26c984568ee3216d2f24a8
alifzaidan <zaidanalif22710@gmail.com>
2024-06-25 19:53:36 +0700

    update kartu peserta

--- resources/views/olimpiade/cetak_kartu/kartu.blade.php
+++ resources/views/olimpiade/cetak_kartu/kartu.blade.php
@@ -7,18 +7,75 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Participant Card</title>
-    @vite('resources/css/app.css')
+    {{-- @vite('resources/css/app.css') --}}
+    <style>
+        @font-face {
+            font-family: 'Andalus';
+            src: url("{{ asset('fonts/andalus.ttf') }}") format("truetype");
+        }
+
+        body {
+            font-family: 'Andalus', sans-serif;
+            font-weight: bold;
+        }
+
+        .card {
+            width: 768px;
+            height: 1086px;
+            background-image: url("{{ asset('img/kartu-peserta.png') }}");
+            background-size: cover;
+            position: relative;
+        }
+
+        .content {
+            width: 100%;
+            display: flex;
+            align-items: center;
+            padding: 4rem
+        }
+
+        .content div {
+            display: flex;
+            flex-direction: column;
+            gap: 0.75rem;
+        }
+
+        .top-content {
+            display: flex;
+            gap: 0.75rem;
+        }
+
+        .content p {
+            margin: 5px 0;
+        }
+
+        .top-data {
+            display: flex;
+            flex-direction: column;
+            gap: 0.75rem;
+        }
+
+        .bottom-content {
+            position: absolute;
+            bottom: 0;
+            left: 0;
+            right: 0;
+            text-align: center;
+            padding: 10px;
+            color: #ffffff;
+        }
+    </style>
 </head>
 
 <body>
-    <div class="w-[768px] h-[1086px] bg-kartu-peserta bg-cover relative font-andalus font-bold">
-        <div class=" h-full flex items-center p-16">
-            <div class="flex flex-col gap-3">
-                <div class="flex gap-3">
+    <div class="card">
+        <div class="content">
+            <div>
+                <div class="top-content">
                     <div>
                         {!! QrCode::size(100)->generate('youtube.com') !!}
                     </div>
-                    <div class="flex flex-col gap-2">
+                    <div class="top-data">
                         <p>Nomer Peserta: {{ $peserta->nomor }}</p>
                         <p>Email Pendaftar: {{ $peserta->email }}</p>
                         <p>Cabang Wilayah: {{ $peserta->cabangs->cabang }}/{{$peserta->rayons->rayon}}</p>
@@ -36,7 +93,7 @@
                 <p>Nomer Telpon/Ponsel Anggota 2: {{ $peserta->telepon_anggota_kedua }}</p>
             </div>
         </div>
-        <p class="absolute bottom-0 text-white text-center p-10">Harap diperhatikan kepada seluruh peserta untuk
+        <p class="bottom-content">Harap diperhatikan kepada seluruh peserta untuk
             mencetak dan
             membawa kartu peserta
             saat babak penyisihan berlangsung</p>
