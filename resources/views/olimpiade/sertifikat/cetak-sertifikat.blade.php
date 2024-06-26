69458c196add5cdfe31cf0e55e996187899493c2
Onny Putra Alamsyah <onnytra@gmail.com>
2024-06-26 18:02:08 +0700

    Sertifikat
    Signed-off-by: Onny Putra Alamsyah <onnytra@gmail.com>

--- resources/views/olimpiade/sertifikat/cetak-sertifikat.blade.php
+++ resources/views/olimpiade/sertifikat/cetak-sertifikat.blade.php
@@ -6,12 +6,41 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Participant Card</title>
     @vite('resources/css/app.css')
+    <style>
+        @font-face {
+            font-family: 'Andalus';
+            src: url("{{ asset('fonts/andalus.ttf') }}") format("truetype");
+        }
+
+        body {
+            font-family: 'Andalus', sans-serif;
+            font-weight: bold;
+        }
+
+        .bg-sertifikat-peserta {
+            background-image: url('{{ asset('img/sertifikat-peserta.png') }}');
+            background-size: cover;
+        }
+
+        @media print {
+            body {
+                -webkit-print-color-adjust: exact;
+                print-color-adjust: exact;
+            }
+
+            .bg-sertifikat-peserta {
+                background-image: url('{{ asset('img/sertifikat-peserta.png') }}') !important;
+                -webkit-print-color-adjust: exact;
+                print-color-adjust: exact;
+            }
+        }
+    </style>
 </head>
 
-<body>
+<body onload="window.print()">
     <div class="w-[1367px] h-[960px] bg-sertifikat-peserta bg-cover relative font-andalus">
         <div class="absolute text-center w-full pl-64 pt-4 top-80 text-4xl ">
-            <p>{{$peserta->nama}}</p>
+            <p>{{ $peserta->nama }}</p>
         </div>
     </div>
 </body>
