e5074c1421c4604bbaa50b7bc27acacf1d536f9e
Onny Putra Alamsyah <onnytra@gmail.com>
2024-07-07 13:47:11 +0700

    Update_DB_Pembayaran
    Signed-off-by: Onny Putra Alamsyah <onnytra@gmail.com>

---
+++ database/migrations/2024_07_07_133504_update_pembayaran.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Schema::table('pembayarans', function (Blueprint $table) {
+        //     $table->text('bukti')->nullable()->change();
+        // });
+        DB::statement('ALTER TABLE pembayarans MODIFY COLUMN bukti TEXT');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // Schema::table('pembayarans', function (Blueprint $table) {
+        //     $table->string('bukti', 50)->nullable()->change();
+        // });
+    }
+};
